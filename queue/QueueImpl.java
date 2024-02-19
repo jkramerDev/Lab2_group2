4e78fdb2cc37b32ef4c97482c92844c68b7caaa8
ViktorNikolov1 <158360283+ViktorNikolov1@users.noreply.github.com>
2024-02-19 17:24:50 -0500

    Update QueueImpl.java

--- queue/QueueImpl.java
+++ queue/QueueImpl.java
@@ -4,7 +4,7 @@ public class QueueImpl implements Queue {
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
+	
 		return false;
 	}
 
@@ -14,6 +14,64 @@ public class QueueImpl implements Queue {
 		return false;
 	}
 
+		
+	public class Queue {
+	    private String[] arr;
+	    private int front;
+	    private int rear;
+	    private int size;
+
+	    public Queue(int capacity) {
+	        arr = new String[capacity];
+	        front = 0;
+	        rear = -1;
+	        size = 0;
+	    }
+
+	    public void enQueue(String element) {
+	        if (size == arr.length) {
+	            throw new IllegalStateException("Queue is full");
+	        }
+	        rear = (rear + 1) % arr.length;
+	        arr[rear] = element;
+	        size++;
+	    }
+
+	    public String deQueue() {
+	        if (isEmpty()) {
+	            throw new IllegalStateException("Queue is empty");
+	        }
+	        String element = arr[front];
+	        front = (front + 1) % arr.length;
+	        size--;
+	        return element;
+	    }
+
+	    public static String pollFirst() {
+	        throw new UnsupportedOperationException("pollFirst() is a static method and cannot be called on an instance of Queue");
+	    }
+
+	    public void display() {
+	        if (isEmpty()) {
+	            System.out.println("Queue is empty");
+	            return;
+	        }
+	        for (int i = front; i != rear; i = (i + 1) % arr.length) {
+	            System.out.print(arr[i] + " ");
+	        }
+	        System.out.println(arr[rear]);
+	    }
+
+	    public String peek() {
+	        if (isEmpty()) {
+	            throw new IllegalStateException("Queue is empty");
+	        }
+	        return arr[front];
+	    }
+	    
+	  }
+
+
 	@Override
 	public void enQueue(String element) {
 		// TODO Auto-generated method stub
@@ -21,9 +79,9 @@ public class QueueImpl implements Queue {
 	}
 
 	@Override
-	public String deQueue() {
+	public void deQueue() {
 		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 	@Override
@@ -38,4 +96,7 @@ public class QueueImpl implements Queue {
 		return null;
 	}
 
-}
+
+		
+	}
+
